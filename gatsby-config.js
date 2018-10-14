4a351f9af27f8b252ac3a953ae5fc4395d8e81ae
CosmicWill <wilfredor@mail.usf.edu>
2018-10-14 17:49:42 -0500

    Updated name, short_name and colors in manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'guady',
+        short_name: 'guady',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'white',
+        theme_color: 'white',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
